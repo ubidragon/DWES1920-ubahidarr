1bc139455353e98b499eb472f6f78eb4c7655622
Ubaldo <ubidragon@protonmail.com>
2020-01-19 14:02:25 +0100

    URI relativa
    Se mete un metodo para detecta la uri en la que nos encontramos y asi poder navegar de manera fluida

--- DWES02/includes/header.inc.php
+++ DWES02/includes/header.inc.php
@@ -3,6 +3,12 @@ date_default_timezone_set('Europe/Madrid');
 setlocale (LC_TIME, "es_ES");
 require_once("funciones.inc.php");
 
+$parent = explode("/", trim(dirname($_SERVER['PHP_SELF'])));
+					$uri = 'http://'.$_SERVER['SERVER_NAME'];
+					array_pop($parent);
+                    foreach($parent as $path){
+                        $uri.=$path."/";
+                    }
 
 
 
@@ -14,16 +20,17 @@ require_once("funciones.inc.php");
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body> 
+<link rel="stylesheet" href="<?php echo $uri.'css/custom.css'?>">
 <form class="formNotBorder">   
 	<?php if (isset($_POST["loginUser"]) && !empty($_POST["loginUser"])) { ?>
 		<input type="hidden" name="loginUser" value="<?php echo $_POST["loginUser"] ?>">
 		<ul class="navbar">
-			<li class="navbarChild"><button type="submit" formmethod="post" formaction="userMenu.php">Menu de usuario</button></li>
-			<li class="navbarChild"><button type="submit" formmethod="post" formaction="ultimosMovs.php">Ultimos movimientos</button></li>
-        	<li class="navbarChild"><button type="submit" formmethod="post" formaction="ingreso.php">Ingresos</button></li>
-        	<li class="navbarChild"><button type="submit" formmethod="post" formaction="gastos.php">Gastos</button></li>
-        	<li class="navbarChild"><button type="submit" formmethod="post" formaction="eliminarMov.php">Eliminar movimientos</button></li>
-			<li class="navbarChild"><a href="../">Cerrar sesión</a></li>		
+			<li class="navbarChild"><button type="submit" formmethod="post" formaction="<?php echo $uri.'user/userMenu.php'; ?>">Menu de usuario</button></li>
+			<li class="navbarChild"><button type="submit" formmethod="post" formaction="<?php echo $uri.'user/ultimosMovs.php'; ?>">Ultimos movimientos</button></li>
+        	<li class="navbarChild"><button type="submit" formmethod="post" formaction="<?php echo $uri.'user/ingreso.php'; ?>">Ingresos</button></li>
+        	<li class="navbarChild"><button type="submit" formmethod="post" formaction="<?php echo $uri.'user/gastos.php'; ?>">Gastos</button></li>
+        	<li class="navbarChild"><button type="submit" formmethod="post" formaction="<?php echo $uri.'user/eliminarMov.php'; ?>">Eliminar movimientos</button></li>
+			<li class="navbarChild"><a href="<?php echo $uri; ?>">Cerrar sesión</a></li>		
 		</ul>
 		<p><i>Usuario: <?php echo $_POST["loginUser"]?></i></p>
 	<?php }
@@ -31,11 +38,11 @@ require_once("funciones.inc.php");
 	?>
 	<input type="hidden" name="admin" value="<?php echo reloadAdmin(); ?>">
 		<ul class="navbar">
-			<li class="navbarChild"><button type="submit" formmethod="post" formaction="gestion.php">Menú de administracion</button></li>
-        	<li class="navbarChild"><button type="submit" formmethod="post" formaction="nuevoUsuario.php">Nuevo usuario</button></li>
-        	<li class="navbarChild"><button type="submit" formmethod="post" formaction="modificaUsuario.php">Modificar usuario</button></li>
-        	<li class="navbarChild"><button type="submit" formmethod="post" formaction="borraUsuario.php">Borrar usuario</button></li>
-			<li class="navbarChild"><button type="submit" formmethod="post" formaction="../">Cerrar sesión</button></li>
+			<li class="navbarChild"><button type="submit" formmethod="post" formaction="<?php echo $uri.'admin/gestion.php'?>">Menú de administracion</button></li>
+        	<li class="navbarChild"><button type="submit" formmethod="post" formaction="<?php echo $uri.'admin/nuevoUsuario.php'?>">Nuevo usuario</button></li>
+        	<li class="navbarChild"><button type="submit" formmethod="post" formaction="<?php echo $uri.'admin/modificaUsuario.php'?>">Modificar usuario</button></li>
+        	<li class="navbarChild"><button type="submit" formmethod="post" formaction="<?php echo $uri.'admin/borraUsuario.php'?>">Borrar usuario</button></li>
+			<li class="navbarChild"><a href="<?php echo $uri; ?>">Cerrar sesión</a></li>		
 		</ul>
 		<p><i>Administrador: <?php echo reloadAdmin()?></i></p>
 <?php
