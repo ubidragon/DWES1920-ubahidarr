ff7f442216ef84931f18a3500742563b7dc13da7
Ubaldo <ubidragon@protonmail.com>
2020-06-02 13:50:26 +0200

    Actualizacion a Bootstrap 4

--- DWES06/HidalgoArriagaUbaldo_DWES06_Tarea_E1-Bootstrap/funciones.js
+++ DWES06/HidalgoArriagaUbaldo_DWES06_Tarea_E1-Bootstrap/funciones.js
@@ -1,27 +1,26 @@
 $(document).ready(function () {
 
   clean();
+ 
   pintaTabla();
-
+  
   $(document).on('click', '#insertButton', (e) => {
-    let element = $(this)[0].activeElement.parentElement.parentElement;
+    let element = this.id;
     modalGenerico(element, 'insert');
   });
 
   $(document).on('click', '.edit', (e) => {
-    let element = $(this)[0].activeElement;
-    let id = $(element).attr('id');
+    let id = this.activeElement.id;
     modalGenerico(id, 'edit');
   });
 
   $(document).on('click', '.delete', (e) => {
-    let element = $(this)[0].activeElement;
-    let id = $(element).attr('id');
+    let id =this.activeElement.id;
     modalGenerico(id, 'delete');
   });
 
   $(document).on('click', '#accionProducto', (e) => {
-    modal.style.display = "none";
+    $('#modal').modal('hide');
     accion = document.getElementsByName("accionProducto")[0].value
     elemento = document.getElementsByClassName("clean");
     // console.log(accion);
@@ -36,19 +35,6 @@ $(document).ready(function () {
 
 function modalGenerico(id, tipo) {
   clean();
-  var span = document.getElementsByClassName("close")[0];
-  var modal = document.getElementById("modal");
-
-  modal.style.display = "block";
-
-  span.onclick = function () {
-    modal.style.display = "none";
-  }
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
 
   if (tipo == 'edit' || tipo == 'delete') {
     $('#name').val($('#name-' + id).text()).prop('disabled', true);
@@ -64,14 +50,13 @@ function modalGenerico(id, tipo) {
   if (tipo == 'delete') {
     $('#stock').prop('disabled', true);
     $("#accionProducto").html('Borrar Producto').val('borrar');
+    $("#modalTitle").html('Borrar Producto');
     $("#msg").html('¿Esta seguro de eliminar este producto?');
   } else if (tipo == 'edit') {
     $("#accionProducto").html('Editar Producto').val('editar');
+    $("#modalTitle").html('Editar Producto').val('editar');
   }
-
-
-
-
+  
 }
 
 function clean() {
@@ -90,13 +75,13 @@ function operaciones(accion, elemento) {
   let stock = "";
 
 
-    for (let item of elemento) {
-      articulo.push(item.value);
-    }
+  for (let item of elemento) {
+    articulo.push(item.value);
+  }
 
   let dataProducto = {
-    "accionProducto" : accion,
-    "articulo" : articulo
+    "accionProducto": accion,
+    "articulo": articulo
   };
 
   $.ajax({
@@ -104,40 +89,38 @@ function operaciones(accion, elemento) {
     data: dataProducto,
     dataType: "json",
     type: 'POST',
-    success: function () { 
+    success: function () {
       pintaTabla();
     }
 
   });
 
-  
+
 }
-
-
 
 function pintaTabla() {
   $.ajax({
     url: 'claseDB.php',
     type: 'POST',
     success: function (response) {
-      
+
       const productos = JSON.parse(response);
       let tabla = '';
 
       productos.forEach(producto => {
         tabla += `
           <tr >
-            <td id="${producto.codigo}">${producto.codigo}</td> 
-            <td id="name-${producto.codigo}">${producto.nombre}</td>
-            <td id="desc-${producto.codigo}">${producto.descripcion}</td> 
-            <td id="pvp-${producto.codigo}">${producto.PVP}</td> 
-            <td id="fam-${producto.codigo}">${producto.familia}</td>
-            <td id="stock-${producto.codigo}">${producto.stock}</td>                    
-            <td style="width: 10%" >
-              <button class="edit" style="width: 48%" type="submit" formmethod="post" name="editButton" id="${producto.codigo}" formaction="">
+            <td scope="row" id="${producto.codigo}">${producto.codigo}</td> 
+            <td scope="row" id="name-${producto.codigo}">${producto.nombre}</td>
+            <td scope="row" id="desc-${producto.codigo}">${producto.descripcion}</td> 
+            <td scope="row" id="pvp-${producto.codigo}">${producto.PVP}</td> 
+            <td scope="row" id="fam-${producto.codigo}">${producto.familia}</td>
+            <td scope="row" id="stock-${producto.codigo}">${producto.stock}</td>                    
+            <td scope="row" style="width: 10%" >
+              <button class="edit" data-toggle="modal" data-target="#modal" type="submit" formmethod="post" name="editButton" id="${producto.codigo}" formaction="">
                 <img style="display: block; margin-left: auto;  margin-right: auto; " src="./include/create.svg" alt="Editar">
               </button>
-              <button class="delete" style="width: 48%" type="submit" name="deleteButton"  id="${producto.codigo}"formmethod="post" formaction="">
+              <button class="delete" data-toggle="modal" data-target="#modal" type="submit" name="deleteButton"  id="${producto.codigo}"formmethod="post" formaction="">
                 <img style="display: block; margin-left: auto;  margin-right: auto; " src="./include/delete.svg" alt="Eliminar">
               </button>
             </td>
@@ -149,63 +132,3 @@ function pintaTabla() {
     }
   });
 }
-
-/**Trash */
-
-// function muestraModal(){
-
-//   var insertBtn = document.getElementById("insertButton");
-//   var editBtn = document.getElementsByClassName("editProduct");
-//   var deleteBtn = document.getElementsByClassName("deleteProduct");;
-
-//   modalGenerico(insertBtn);
-
-//   for (let item of editBtn) {
-//     modalGenerico(document.getElementById(item.id));
-//   }
-
-//   for (let item of deleteBtn) {
-//     modalGenerico(document.getElementById(item.id));
-//   }
-
-// }
-
-//     $('#task-form').submit(e => {
-//       e.preventDefault();
-//       const postData = {
-//         name: $('#name').val(),
-//         description: $('#description').val(),
-//         id: $('#taskId').val()
-//       };
-//       const url = edit === false ? 'task-add.php' : 'task-edit.php';
-//       console.log(postData, url);
-//       $.post(url, postData, (response) => {
-//         console.log(response);
-//         $('#task-form').trigger('reset');
-//         fetchTasks();
-//       });
-//     });
-
-// Get a Single Task by Id 
-// $(document).on('click', '.task-item', (e) => {
-//   const element = $(this)[0].activeElement.parentElement.parentElement;
-//   const id = $(element).attr('taskId');
-//   $.post('task-single.php', {id}, (response) => {
-//     const task = JSON.parse(response);
-//     $('#name').val(task.name);
-//     $('#description').val(task.description);
-//     $('#taskId').val(task.id);
-//     edit = true;
-//   });
-//   e.preventDefault();
-// });
-//     // Delete a Single Task
-//     $(document).on('click', '.task-delete', (e) => {
-//       if(confirm('Are you sure you want to delete it?')) {
-//         const element = $(this)[0].activeElement.parentElement.parentElement;
-//         const id = $(element).attr('taskId');
-//         $.post('task-delete.php', {id}, (response) => {
-//           fetchTasks();
-//         });
-//       }
-//     });
--- DWES06/HidalgoArriagaUbaldo_DWES06_Tarea_E1-Bootstrap/index.php
+++ DWES06/HidalgoArriagaUbaldo_DWES06_Tarea_E1-Bootstrap/index.php
@@ -2,20 +2,28 @@
 ?>
 
 <!DOCTYPE html>
-
 <html>
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<!--     <link rel="stylesheet" type="text/css" href="custom.css" /> -->
+    <!--     <link rel="stylesheet" type="text/css" href="custom.css" /> -->
     <script src="./include/jquery-3.3.1.min.js"></script>
     <!-- CSS only -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-<!-- JS, Popper.js, and jQuery -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!-- JS, Popper.js, and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js"
+        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
+    <!-- DataTables -->
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js">
+    </script>
     <script src="funciones.js"></script>
     <title>AJAX - DWES06</title>
 </head>
@@ -53,28 +61,69 @@
             </div>
             <br />
             <div class="container">
-                <label id="msg"></label>
-                <button class="buttonCenter" type="submit" name="accionProducto" id="accionProducto"
-                    value="insertar">Insertar
-                    Producto</button>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <label for="filter-cod"><b>Codigo</b></label>
+                    <input class="form-control clean" type="text" id="filter-cod" name="filter-cod" maxlength="12"
+                        placeholder="Codigo del producto">
+                </div>
+                <div class="col-md-4">
+                    <label for="filter-name"><b>Nombre</b></label>
+                    <input class="form-control clean" type="text" id="filter-name" name="filter-name" maxlength="12"
+                        placeholder="Nombre del producto">
+                </div>
+                <div class="col-md-4">
+                    <label for="filter-desc"><b>Descripcion</b></label>
+                    <input class="form-control clean" type="text" id="filter-desc" name="filter-descripcion"
+                        maxlength="12" placeholder="Descripcion del producto">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <label for="filter-pvp"><b>PVP</b></label>
+                    <input class="form-control clean" type="number" id="filter-pvp" min="0" step="0.01"
+                        placeholder="PVP" name="filter-pvp" pattern="^[0-9]*\,[0-9][0-9]$" required>
+                </div>
+
+                <div class="col-md-4">
+                    <label for="filter-fam"><b>Familia</b></label>
+                    <input class="form-control clean" type="text" id="filter-fam" name="filter-fam" maxlength="6">
+                </div>
+                <div class="col-md-4">
+                    <label for="filter-stock"><b>Stock</b></label>
+                    <input class="form-control clean" type="number" id="filter-stock" min="0" step="1"
+                        placeholder="Stock" name="filter-stock" pattern="^[0-9]*\,[0-9][0-9]$" required>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-md-4 margin-md-6">
+                </div>
+
+                <div class="col-md-4">
+                    <button type="button" class="btn btn-primary  btn-block">Busqueda Avanzada</button>
+                </div>
             </div>
         </div>
+    </form>
+    <br>
+    <div class="container-fluid">
+        <table id="tablaProductos" class=" table-striped  table-bordered">
+            <thead>
+                <tr>
+                    <th scope="col">Código</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Descripcion</th>
+                    <th scope="col">PVP</th>
+                    <th scope="col">Familia</th>
+                    <th scope="col">Stock</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody id="productos"></tbody>
+        </table>
     </div>
-    <table>
-        <thead>
-            <tr>
-                <th>Código</th>
-                <th>Nombre</th>
-                <th>Descripcion</th>
-                <th>PVP</th>
-                <th>Familia</th>
-                <th>Stock</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody id="productos"></tbody>
-    </table>
-
 </body>
 
 </html>
