9b6bf5a699b982b9452fd0cb0ef5760600a43cd9
Ubaldo <ubidragon@protonmail.com>
2020-06-02 13:51:30 +0200

    Actualizacion del Modal de productos y el navbar

--- DWES06/HidalgoArriagaUbaldo_DWES06_Tarea_E1-Bootstrap/index.php
+++ DWES06/HidalgoArriagaUbaldo_DWES06_Tarea_E1-Bootstrap/index.php
@@ -29,38 +29,99 @@
 </head>
 
 <body>
-    <div class="topnav">
-        <div class="search-container">
-            <button id="insertButton">Insertar Producto</button>
-            <!-- <input id="buscar" type="text" placeholder="Buscar.." name="buscar">
-                <button type="submit">Buscar</button>
-                <div id="producto-result">
-                    <ul id="container"></ul>
-                </div> -->
+    <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalTitle">Insertar Producto</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <label for="cod"><b>Codigo</b></label>
+                                <input class="form-control clean" type="text" id="cod" name="cod" maxlength="12">
+                            </div>
+                            <div class="col-md-8">
+                                <label for="name"><b>Nombre</b></label>
+                                <input class="form-control clean" type="text" id="name" name="name" maxlength="12">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <label for="desc"><b>Descripcion</b></label>
+                                <input class="form-control clean" type="text" id="desc" name="descripcion"
+                                    maxlength="12">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-4">
+                                <label for="pvp"><b>PVP</b></label>
+                                <input class="form-control clean" type="number" id="pvp" min="0" step="0.01"
+                                    placeholder="PVP" name="pvp" pattern="^[0-9]*\,[0-9][0-9]$" required>
+                            </div>
+
+                            <div class="col-md-4">
+                                <label for="fam"><b>Familia</b></label>
+                                <input class="form-control clean" type="text" id="fam" name="fam" maxlength="6">
+                            </div>
+                            <div class="col-md-4">
+                                <label for="stock"><b>Stock</b></label>
+                                <input class="form-control clean" type="number" id="stock" min="0" step="1"
+                                    placeholder="Stock" name="stock" pattern="^[0-9]*\,[0-9][0-9]$" required>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-2">
+
+                            </div>
+                            <div class="col-md-10">
+                                <p class="text-danger" id="msg"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    <button type="submit" name="accionProducto" id="accionProducto" value="insertar"
+                        class="btn btn-primary">Insertar Producto</button>
+                </div>
+            </div>
         </div>
     </div>
-    <div id="modal" class="modal">
-        <div class="modal-content">
-            <span class="close">&times;</span>
-            <div class="container">
-        
-                <label for="cod"><b>Codigo</b></label>
-                <input class="clean" type="text" id="cod" name="cod" maxlength="12">
-                <label for="name"><b>Nombre</b></label>
-                <input class="clean" type="text" id="name" name="name" maxlength="12">
-                <label for="desc"><b>Descripcion</b></label>
-                <input class="clean" type="text" id="desc" name="descripcion" maxlength="12">
-                <label for="pvp"><b>PVP</b></label>
-                <input class="clean" type="number" id="pvp" min="0" step="0.01" placeholder="PVP" name="pvp"
-                    pattern="^[0-9]*\,[0-9][0-9]$" required>
-                <label for="fam"><b>Familia</b></label>
-                <input class="clean" type="text" id="fam" name="fam" maxlength="6">
-                <label for="stock"><b>Stock</b></label>
-                <input class="clean" type="number" id="stock" min="0" step="1" placeholder="Stock" name="stock"
-                    pattern="^[0-9]*\,[0-9][0-9]$" required>
-            </div>
             <br />
             <div class="container">
+    <div class="d-flex justify-content-center bg-dark text-warning">
+        <h1>Amazonia</h1>
+    </div>
+    <nav class="navbar navbar-light bg-light justify-content-between">
+        <div class="form-inline">
+            <div class="row">
+                <div class="col-md-4">
+                    <button class="btn btn-outline-info" data-toggle="modal"
+                        data-target="#modalChangeLog">Changelog</button></span>
+                </div>
+                <div class="col-md-8">
+                    <button id="insertButton" class="btn btn-outline-success" data-toggle="modal"
+                        data-target="#modal">Insertar Producto</button>
+                </div>
+                <div class="col-md-4">
+
+                </div>
+            </div>
+        </div>
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+        </form>
+    </nav>
+    <form>
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
